b42fb9f789dbe5e44bc516f594a5f92c3d330305
starunz <busouza01@gmail.com>
2022-03-21 21:16:28 -0500

    initial structure

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express, { json } from "express";
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config()
+
+import router from "./routes/index.js";
+
+const app = express()
+app.use(cors());
+app.use(json());
+
+app.use(router);
+
+app.listen(process.env.PORT, () => {
+    console.log(`rodando na porta ${process.env.PORT}`);
+});
---
+++ src/middlewares/validateSchema.js
@@ -0,0 +1,16 @@
+function validateSchema(schema) {
+
+    return (req, res, next) => {
+      const validation = schema.validate(req.body, { abortEarly: false });
+  
+      if (validation.error) {
+        const errors = validation.error.details.map(detail => detail.message);
+  
+        return res.status(422).send(errors);
+      }
+      
+      next();
+    };
+}
+
+export default validateSchema;
