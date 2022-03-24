6d9d3fd26a489d65e0da33f638a03963d95b726c
Mateus <mateusc.rossetto@hotmail.com>
2022-03-24 10:56:43 -0300

    fix: repeat hashtags

--- src/controllers/hashtagsController.js
+++ src/controllers/hashtagsController.js
@@ -3,11 +3,19 @@ import connection from '../db.js';
 export async function insertHashtags(hashtags, postId){
     for (const hashtag of hashtags) {
 
-        await connection.query(`
-            INSERT INTO hashtags 
-            (name) 
-            VALUES ($1)
-        `, [hashtag]);
+        try {
+            await connection.query(`
+                INSERT INTO hashtags 
+                (name) 
+                VALUES ($1)
+            `, [hashtag]);
+        } catch (error) {
+            if(error.message === 'duplicate key value violates unique constraint "hashtags_name_key"'){
+                continue;
+            }else {
+                return error.message;
+            }
+        }
 
         const {rows: hashtagId} = await connection.query(`
             SELECT MAX(id) FROM hashtags
