a2f7bce3b38b345fbe23b4d9460378a8ea4c9ed9
Mateus <mateusc.rossetto@hotmail.com>
2022-03-23 23:45:00 -0300

    feat: get posts

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,4 +1,5 @@
 import connection from '../db.js';
+import urlMetadata from 'url-metadata';
 
 export async function publishPosts(req, res) {
 
@@ -16,4 +17,34 @@ export async function publishPosts(req, res) {
         console.log(error.message);
         res.sendStatus(500);
     }
+}
+
+export async function getPosts(req, res) {
+
+    try {
+        const {rows: posts} = await connection.query(`
+            SELECT 
+                p.description, u."userName" author, u."photoUrl", p.link
+            FROM posts p
+            LEFT JOIN users u on p."userId" = u.id 
+            GROUP BY description, author, "photoUrl", p.id
+            ORDER BY p.id DESC
+            LIMIT 20
+        `);
+
+        for (const post of posts) {
+            const {image, title, description} = await urlMetadata(post.link);
+
+            post.linkImage = image;
+            post.linkTitle = title;
+            post.linkDescription = description;
+        }
+        
+        res.send(posts);
+
+    } catch (error) {
+        console.log(error.message);
+        res.sendStatus(500);
+    }
+
 }
--- src/routes/postsRouter.js
+++ src/routes/postsRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { publishPosts } from "../controllers/postsController.js";
+import { getPosts, publishPosts } from "../controllers/postsController.js";
 import validateSchemaMiddleware from "../middlewares/validateSchemaMiddleware.js";
 import { postsSchemas } from "../schemas/postsSchema.js";
 
 const postsRouter = Router();
 postsRouter.post('/posts', validateSchemaMiddleware(postsSchemas), publishPosts);
+postsRouter.get('/posts', getPosts);
 
 export default postsRouter;
