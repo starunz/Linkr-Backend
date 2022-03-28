1d592909e3bb1994016be474162cfa243aa04912
starunz <busouza01@gmail.com>
2022-03-28 12:08:58 -0500

    schema additions

--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -1,10 +1,10 @@
 import joi from "joi";
 
 const userSchema = joi.object({
-  email: joi.string().email().required(),
-  password: joi.string().required(),
-  username: joi.string().required(),
-  photoUrl: joi.string().uri().required(),
+  email: joi.string().email().trim().required(),
+  password: joi.string().trim().required(),
+  username: joi.string().min(3).trim().required(),
+  photoUrl: joi.string().uri().trim().required(),
 });
 
 export default userSchema;
