e2e251f2d259ff9ed8a51d4bb7efde81758f8499
Mateus <mateusc.rossetto@hotmail.com>
2022-03-30 02:12:07 -0300

    fix: double spaces on descriptions of posts

--- src/utilityFunctions.js
+++ src/utilityFunctions.js
@@ -3,6 +3,7 @@ export default function addSpaceHashtagsStuck(text){
     if(text.includes('#')){
         text = text.replace(/#/gi, ' #');
         text = text.replace(/# /gi, '');
+        text = text.replace(/\s{2,}/g, ' ');
         text = text.trim();
     }
     
